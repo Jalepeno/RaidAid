7b15360cf93596a1037ac86bc659703841ce6c7f
Nicolaj Pededersen <nicolajsp@live.dk>
2015-05-01 20:07:52 +0200

    ting sker igen

---
+++ app/src/main/java/dk/nicolajpedersen/raidaid/Data/Membership.java
@@ -0,0 +1,35 @@
+package dk.nicolajpedersen.raidaid.Data;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ * Created by Nicolaj on 29-04-2015.
+ */
+public class Membership extends User {
+    int rank;
+    String clanID;
+    public int getRank() {
+        return rank;
+    }
+
+    public void setRank(int rank) {
+        this.rank = rank;
+    }
+
+    public Membership(String userName, String userID) {
+        super(userName, userID);
+    }
+
+    public Membership(JSONObject jsonObject){
+        super(jsonObject);
+        try {
+            clanID = jsonObject.getString("ClanID");
+            rank = jsonObject.getInt("Rank");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+}
